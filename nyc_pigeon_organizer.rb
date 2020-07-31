61fd502880e58c1a4635ea98b33590dbc77969b9
Islom Mashanlo <islom.mashanlo@gmail.com>
2020-07-31 14:20:45 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  new_hash = {}
+  data.each do |attribs, value|
+    value.each do |stats, pigeon_names|
+      pigeon_names.each do |name|
+        if new_hash[name] == nil
+          new_hash[name] = {}
+        end
+        if new_hash[name][attribs] == nil
+          new_hash[name][attribs] = []
+        end
+        new_hash[name][attribs].push(stats.to_s)
+      end
+    end
+  end
+  new_hash
 end
